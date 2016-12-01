15f77413915ff51d4fa0ae5e03f2ded52ead01f1
David <tr.ywchiu@gmail.com>
2016-12-01 15:58:48 +0800

    add script

--- HiveAdvanced/RunningCustomPythonscript.sql
+++ HiveAdvanced/RunningCustomPythonscript.sql
@@ -1,4 +1,18 @@
+CREATE TABLE employee
+(
+firstname VARCHAR(30),
+lastname VARCHAR(30));
 
-add file '/Users/swethakolalapudi/function.py'
+Insert into employee
+Values
+('Bellandur','Nutella'      ),
+('Bellandur','Peanut Butter'),
+('Bellandur','Milk'         ),
+('Koram'    ,'Bananas'      ),
+('Koram'    ,'Nutella '     ),
+('Koram'    ,'Peanut Butter'),
+('Koram'    ,'Milk'         );
 
-SELECT TRANSFORM(firstname,lastname) USING 'python function.py' from employees; 
+add FILE /root/function.py'
+
+SELECT TRANSFORM(firstname,lastname) USING 'python function.py' from employee; 
